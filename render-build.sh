586ce8f11be9a9ab386d63eca694adcb04f4249c
Nil Vaghani <198755973+Nil-Vaghani@users.noreply.github.com>
2026-02-28 15:19:47 +0530

    remove render-build.sh, use npm install directly as build cmd

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Render build script: installs Node dependencies
-# Puppeteer will download its own bundled Chromium during npm install
-
-set -e
-
-# Install Node dependencies
-npm install
